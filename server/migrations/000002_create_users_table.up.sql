a9a4843bc5a21b97bc26079efc66e7ac80ff831c
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-10-06 19:42:36 -0600

    feat: add database migrations for creating users table

---
+++ server/migrations/000002_create_users_table.up.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE
+  IF NOT EXISTS users (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    email text NOT NULL UNIQUE,
+    hashedPassword text NOT NULL,
+    created_at timestamp
+    with
+      time zone DEFAULT now (),
+      updated_at timestamp
+    with
+      time zone DEFAULT now ()
+  );
