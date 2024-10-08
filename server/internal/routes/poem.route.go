b24d4b3472a5f0bf1f1bc8caaef1eb117cd854c9
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-10-07 22:04:56 -0600

    refactor: move poem route handlers to controller

--- server/internal/routes/poem.route.go
+++ server/internal/routes/poem.route.go
@@ -1,27 +1,19 @@
 package routes
 
 import (
-	"net/http"
+	"rime-server/internal/controllers"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func RegisterPoemRoutes(r *chi.Mux) {
+	poemController := &controllers.PoemController{}
+
 	r.Group(func(r chi.Router) {
-		r.Get("/poems", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Poems"))
-		})
-		r.Get("/poems/{id}", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Poem"))
-		})
-		r.Post("/poems", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Create Poem"))
-		})
-		r.Put("/poems/{id}", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Update Poem"))
-		})
-		r.Delete("/poems/{id}", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Delete Poem"))
-		})
+		r.Get("/poems", poemController.GetPoems)
+		r.Get("/poems/{id}", poemController.GetPoem)
+		r.Post("/poems", poemController.CreatePoem)
+		r.Put("/poems/{id}", poemController.UpdatePoem)
+		r.Delete("/poems/{id}", poemController.DeletePoem)
 	})
 }
