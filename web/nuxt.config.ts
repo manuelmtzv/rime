4c10e8be570fe5aaf0476b4bf76228bcb9eb74e4
Manuel Martínez <104857693+manuelmtzv@users.noreply.github.com>
2024-11-04 20:48:01 +0000

    feat: add Pinia for state management and update dependencies in configuration files

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/google-fonts", "@nuxt/icon"],
+  modules: ["@nuxt/ui", "@nuxtjs/google-fonts", "@nuxt/icon", "@pinia/nuxt"],
   googleFonts: {
     families: {
       Merriweather: true,
