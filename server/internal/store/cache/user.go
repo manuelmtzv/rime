c215492a956aa3962701519b6d14e1f951f30382
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-11-04 07:25:21 -0600

    feat: enhance UserStore methods to use formatted keys and add expiration for user cache

--- server/internal/store/cache/user.go
+++ server/internal/store/cache/user.go
@@ -3,7 +3,9 @@ package cache
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"rime-api/internal/models"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -12,38 +14,41 @@ type UserStore struct {
 	rdb *redis.Client
 }
 
-func (s *UserStore) Get(ctx context.Context, id string) (*models.User, error) {
-	cmd := s.rdb.Get(ctx, id)
+const UserExpTime = time.Hour * 2
 
-	if cmd.Err() != nil {
-		return nil, cmd.Err()
+func (s *UserStore) Get(ctx context.Context, id string) (*models.User, error) {
+	userKey := fmt.Sprintf("user-%s", id)
+
+	dataString, err := s.rdb.Get(ctx, userKey).Result()
+	if err == redis.Nil {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
 	}
 
 	var u models.User
-
-	err := json.Unmarshal([]byte(cmd.Val()), &u)
-	if err != nil {
-		return nil, err
+	if dataString != "" {
+		err := json.Unmarshal([]byte(dataString), &u)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return &u, nil
 }
 
 func (s *UserStore) Set(ctx context.Context, user *models.User) error {
-	b, err := json.Marshal(user)
+	userKey := fmt.Sprintf("user-%s", user.ID)
+
+	json, err := json.Marshal(user)
 	if err != nil {
 		return err
 	}
 
-	cmd := s.rdb.Set(ctx, user.ID, b, 0)
-
-	if cmd.Err() != nil {
-		return cmd.Err()
-	}
-
-	return nil
+	return s.rdb.Set(ctx, userKey, json, UserExpTime).Err()
 }
 
 func (s *UserStore) Delete(ctx context.Context, id string) {
-	s.rdb.Del(ctx, id)
+	userKey := fmt.Sprintf("user-%s", id)
+	s.rdb.Del(ctx, userKey)
 }
