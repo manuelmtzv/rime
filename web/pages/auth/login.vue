dd6a9d310b0741ca88d27f974f9fd26eca591cec
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-11-04 23:18:25 -0600

    feat: add register form base structure

--- web/pages/auth/login.vue
+++ web/pages/auth/login.vue
@@ -6,7 +6,7 @@ function handleSubmit() {
 
 <template>
   <Page class="">
-    <form class="auth-form mt-20 mb-10" @submit.prevent="handleSubmit">
+    <form class="auth-form" @submit.prevent="handleSubmit">
       <div class="text-center space-y-2 mb-2">
         <h2 class="font-semibold text-lg">Welcome back!</h2>
         <p>Enter your credentials to access your writings</p>
@@ -16,12 +16,12 @@ function handleSubmit() {
 
       <UInput size="md" placeholder="Your password" />
 
-      <div class="flex justify-between items-center">
-        <ULink href="/auth/register" class="text-sm"
-          >Don't have an account?</ULink
-        >
+      <div class="flex justify-between items-center my-1">
+        <span class="text-sm">Don't have an account?</span>
 
-        <ULink href="/auth/register" class="text-sm">Create an account</ULink>
+        <NuxtLink to="/auth/register" class="text-sm"
+          >Create an account</NuxtLink
+        >
       </div>
 
       <UButton type="submit" class="justify-center font-semibold" size="md"
--- web/pages/auth/register.vue
+++ web/pages/auth/register.vue
@@ -1,1 +1,65 @@
-<template>Register</template>
+<script setup lang="ts">
+const showPassword = ref(false);
+
+const form = reactive({
+  name: "",
+  email: "",
+  username: "",
+  password: "",
+  confirmPassword: "",
+});
+
+function handleSubmit() {
+  console.log("submit");
+}
+
+function togglePasswordVisibility() {
+  showPassword.value = !showPassword.value;
+}
+</script>
+
+<template>
+  <Page class="">
+    <form class="auth-form" @submit.prevent="handleSubmit">
+      <div class="text-center space-y-2 mb-2">
+        <h2 class="font-semibold text-lg">Join the community!</h2>
+        <p>Create an account to start writing</p>
+      </div>
+      <UInput size="md" placeholder="Name" v-model="form.name" />
+
+      <UInput size="md" placeholder="Username" v-model="form.username" />
+
+      <UInput size="md" placeholder="Email" v-model="form.email" />
+
+      <UInput size="md" placeholder="Password" v-model="form.password" />
+
+      <UInput
+        size="md"
+        placeholder="Confirm password"
+        :type="showPassword ? 'text' : 'password'"
+        :ui="{ icon: { trailing: { pointer: '' } } }"
+        v-model="form.confirmPassword"
+      >
+        <template #trailing>
+          <UButton
+            size="sm"
+            variant="ghost"
+            :padded="false"
+            :icon="showPassword ? 'heroicons:eye' : 'heroicons:eye-slash'"
+            @click.prevent="togglePasswordVisibility"
+          ></UButton>
+        </template>
+      </UInput>
+
+      <div class="flex justify-between items-center my-1">
+        <span class="text-sm">Already have an account?</span>
+
+        <NuxtLink to="/auth/login" class="text-sm">Go to login</NuxtLink>
+      </div>
+
+      <UButton type="submit" class="justify-center font-semibold" size="md"
+        >Register</UButton
+      >
+    </form>
+  </Page>
+</template>
