5b1d95e92550fc0b65f35c4e2b37c0be5811ba72
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-11-06 23:40:33 -0600

    feat: extend writing entry structure

--- web/components/writing/Entry.vue
+++ web/components/writing/Entry.vue
@@ -11,16 +11,28 @@ const author = computed(() => props.writing.author);
 </script>
 
 <template>
-  <article class="max-w-xl mx-auto py-6 space-y-4 border-b">
-    <h2 class="text-2xl font-bold font-poetry">{{ writing.title }}</h2>
+  <article class="max-w-xl mx-auto py-6 flex flex-col gap-4 border-b">
+    <NuxtLink :to="`/writings/${writing.id}`">
+      <h2 class="text-2xl font-bold font-poetry mb-4">{{ writing.title }}</h2>
 
-    <TiptapContent class="font-poetry" :content="writing.text" />
+      <TiptapContent class="font-poetry" :content="writing.text" />
+    </NuxtLink>
 
-    <div>
+    <div class="flex justify-between items-center gap-4 mt-2">
       <div v-if="author" class="space-x-2">
         <UAvatar :alt="`${author.name} ${author?.lastname}`" size="sm" />
         <span class="text-sm">{{ `${author.name} ${author?.lastname}` }}</span>
       </div>
+
+      <nav class="flex gap-2">
+        <button>
+          <Icon name="heroicons:heart" class="w-6 h-6" />
+        </button>
+
+        <button>
+          <Icon name="heroicons:share" class="w-6 h-6" />
+        </button>
+      </nav>
     </div>
   </article>
 </template>
