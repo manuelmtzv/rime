5fc30956ebace6cd1038d56a62cae82fdaf3ab9c
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-11-09 00:15:41 -0600

    style: enhance header shadow for improved visibility

--- web/components/app/Header.vue
+++ web/components/app/Header.vue
@@ -2,7 +2,7 @@
 
 <template>
   <header
-    class="pt-7 pb-5 shadow-sm border-b border-transparent dark:border-gray-50"
+    class="pt-7 pb-5 shadow-sm shadow-gray-200 border-b border-transparent dark:border-gray-50 dark:shadow-none"
   >
     <div class="container flex items-center justify-between">
       <NuxtLink to="/">
