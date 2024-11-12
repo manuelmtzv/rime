3fb8c884189f907ceb2dcb8ddcaf49a12a922eea
manuelmtzv <manuel.mtzv816@gmail.com>
2024-11-12 14:25:51 -0600

    feat: add loading state to popular tags

--- web/components/popular/Tags.vue
+++ web/components/popular/Tags.vue
@@ -1,27 +1,43 @@
 <script setup lang="ts">
 import { tagRepository } from "@/repositories/tag.repository";
 
-const { data } = await useAsyncData("popular-tags", tagRepository().getTags);
+const { data, status, error } = await useAsyncData("popular-tags", tagRepository().getTags);
 </script>
 
 <template>
   <Card class="flex flex-col gap-2">
-    <template v-if="data">
-      <div class="inline-flex gap-2 items-center mb-1">
-        <Icon name="heroicons:arrow-trending-up" />
-        <h3>{{ $t("tag.popular.title") }}</h3>
-      </div>
+    <div class="inline-flex gap-2 items-center mb-1">
+      <Icon name="heroicons:arrow-trending-up" />
+      <h3>{{ $t("tag.popular.title") }}</h3>
+    </div>
 
-      <ul class="space-y-1">
+    <ul class="space-y-1">
+      <template v-if="data">
         <li v-for="tag in data.data">
           <a href="#" class="tag">#{{ tag.name }}</a>
         </li>
-      </ul>
-    </template>
+      </template>
+
+      <template v-if="status == 'pending'">
+        <li>
+          <Skeleton class="w-20 h-4" />
+        </li>
+        <li>
+          <Skeleton class="w-20 h-4" />
+        </li>
+        <li>
+          <Skeleton class="w-20 h-4" />
+        </li>
+      </template>
+    </ul>
+
+    <div v-if="status == 'error'">
+      <pre>{{ error }}</pre>
+    </div>
   </Card>
 </template>
 
-<style scoped>
+<style scoped lang="postcss">
 .tag {
   @apply text-sm hover:underline;
 }
