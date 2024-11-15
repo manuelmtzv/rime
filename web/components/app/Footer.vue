6c68e3daf1d6265d00eaae4adf5357db3fa7838d
manuelmtzv <manuel.mtzv816@gmail.com>
2024-11-15 16:23:44 -0600

    feat: add donation link in footer

--- web/components/app/Footer.vue
+++ web/components/app/Footer.vue
@@ -4,10 +4,18 @@ const year = new Date().getFullYear();
 
 <template>
   <footer class="py-6">
-    <div class="container">
-      <p class="text-center text-sm text-gray-500">
+    <div class="container flex items-center gap-4 justify-center  text-sm text-gray-500">
+      <p class="text-center">
         &copy; {{ year }} Rime. {{ $t("app.footer.rights") }}
       </p>
+
+      <span>-</span>
+
+      <a href="https://www.paypal.com/donate/?hosted_button_id=WJ3U5GF2M25XL" target="_blank"
+        class=" px-2 py-0.5 border dark:border-gray-400 rounded-md inline-flex items-center gap-1 hover:border-gray-300 hover:dark:border-gray-50 transition-colors duration-300">
+        <Icon name="heroicons:heart" class="text-red-500" size="16" />
+        Donar
+      </a>
     </div>
   </footer>
 </template>
