3372a6b31e260e5bee42d9195a39bd134092e5a1
manuelmtzv <manuel.mtzv816@gmail.com>
2024-11-17 16:21:43 -0600

    feat: add functionality to check for existing likes before inserting new ones in LikeStore

--- server/internal/store/like.go
+++ server/internal/store/like.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"rime-api/internal/models"
 )
 
@@ -14,6 +15,17 @@ func (s LikeStore) LikeWriting(ctx context.Context, like *models.Like, writingID
 	tx, err := s.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
+	}
+
+	existingLike, err := s.FindOneWritingLikeByUser(ctx, like.UserID)
+	if err != nil && err != sql.ErrNoRows {
+		return err
+	}
+
+	if existingLike != nil {
+		like.ID = existingLike.ID
+		like.CreatedAt = existingLike.CreatedAt
+		return nil
 	}
 
 	query := `
@@ -44,6 +56,17 @@ func (s LikeStore) LikeComment(ctx context.Context, like *models.Like, commentID
 		return err
 	}
 
+	existingLike, err := s.FindOneCommentLikeByUser(ctx, like.UserID)
+	if err != nil && err != sql.ErrNoRows {
+		return err
+	}
+
+	if existingLike != nil {
+		like.ID = existingLike.ID
+		like.CreatedAt = existingLike.CreatedAt
+		return nil
+	}
+
 	query := `
 		INSERT INTO likes (author_id)
 		VALUES ($1)
@@ -64,6 +87,30 @@ func (s LikeStore) LikeComment(ctx context.Context, like *models.Like, commentID
 	}
 
 	return tx.Commit()
+}
+
+func (s LikeStore) findOneLikeByUser(ctx context.Context, userID, joinTable string) (*models.Like, error) {
+	query := fmt.Sprintf(`
+        SELECT l.id, l.created_at
+        FROM likes l
+        JOIN %s ON l.id = %s.like_id
+        WHERE l.author_id = $1
+    `, joinTable, joinTable)
+
+	var like models.Like
+	if err := s.db.QueryRowContext(ctx, query, userID).Scan(&like.ID, &like.CreatedAt); err != nil {
+		return nil, err
+	}
+
+	return &like, nil
+}
+
+func (s LikeStore) FindOneWritingLikeByUser(ctx context.Context, userID string) (*models.Like, error) {
+	return s.findOneLikeByUser(ctx, userID, "like_writing")
+}
+
+func (s LikeStore) FindOneCommentLikeByUser(ctx context.Context, userID string) (*models.Like, error) {
+	return s.findOneLikeByUser(ctx, userID, "like_comment")
 }
 
 func (s LikeStore) Delete(ctx context.Context, likeID string) error {
